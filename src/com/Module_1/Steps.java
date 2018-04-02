289c2e5f2da1431a910f4d73b95301ad5cbda4bb
Mishev <m.n.mishev@gmail.com>
2018-04-03 00:23:49 +0300

    Solved Big_Numbers

---
+++ src/com/Module_1/Steps.java
@@ -0,0 +1,11 @@
+package com.Module_1;
+
+import java.util.Scanner;
+
+public class Steps {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        System.out.println(number*1609);
+    }
+}
